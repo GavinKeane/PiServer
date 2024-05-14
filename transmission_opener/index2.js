80cb2fb9565591e22e80c74d5c18b49ec64428a8
GavinKeane <gavinkeane42@gmail.com>
2024-05-14 14:58:36 -0400

    30s delay to opening transmission

--- transmission_opener/index2.js
+++ transmission_opener/index2.js
@@ -2,5 +2,5 @@ const { execSync } = require('child_process');
 trans_status = execSync("sudo /home/gavin/Documents/project/check-trans.sh", { timeout: 10000 }).toString();
 console.log(`Transmission open? ${trans_status}`)
 if (trans_status.includes("tno")) {
-  execSync("transmission-gtk&");
+  execSync("sleep 30 && transmission-gtk&");
 }
